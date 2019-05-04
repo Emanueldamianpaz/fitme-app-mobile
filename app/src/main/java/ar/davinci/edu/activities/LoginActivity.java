c292bb51c68efbff04d6fa65e7598f6efc4d70f0
Emanuel <emanueldamian.paz@gmail.com>
2019-05-04 18:32:57 -0300

    Added oauth2 to project... remains persist and filter request jwt!

--- app/src/main/java/ar/davinci/edu/activities/LoginActivity.java
+++ app/src/main/java/ar/davinci/edu/activities/LoginActivity.java
@@ -1,69 +1,80 @@
 package ar.davinci.edu.activities;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import ar.davinci.edu.R;
-import ar.davinci.edu.infraestructure.api.ApiClient;
 
 public class LoginActivity extends AppCompatActivity {
     private Context context;
-
+    private Auth0 account;
     private SharedPreferences sharedPreferences;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        gson = new GsonBuilder().create();
+
         context = getBaseContext();
-
-        final ApiClient apiClient = new ApiClient(getBaseContext());
-
-        final TextView usernameTxt = (TextView) findViewById(R.id.txtUsername);
-        final TextView passwordTxt = (TextView) findViewById(R.id.txtPassword);
-        final CheckBox saveUserCheck = (CheckBox) findViewById(R.id.chkSaveUser);
-        Button loginBtn = (Button) findViewById(R.id.btnLogin);
-
+        account = new Auth0(context);
         sharedPreferences = context.getSharedPreferences(getResources().getString(R.string.app_name), MODE_PRIVATE);
 
-        String username = sharedPreferences.getString("username", "");
-        String password = sharedPreferences.getString("password", "");
 
-        if (username != "" && password != "") {
-            usernameTxt.setText(username);
-            passwordTxt.setText(password);
-        }
-
+        Button loginBtn = (Button) findViewById(R.id.btnLogin);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(context, "Intentando conectar...", Toast.LENGTH_LONG).show();
-                if (apiClient.getUser(usernameTxt.getText().toString(), passwordTxt.getText().toString())) {
+                final Intent intent = new Intent(context, HomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                    if (saveUserCheck.isChecked()) {
+                Toast.makeText(context, "Iniciando sesión...", Toast.LENGTH_LONG).show();
 
-                        sharedPreferences = context.getSharedPreferences(getResources().getString(R.string.app_name), MODE_PRIVATE);
-                        sharedPreferences.edit()
-                                .putString("username", usernameTxt.getText().toString())
-                                .putString("password", passwordTxt.getText().toString())
-                                .apply();
-                    }
-                    Intent intent = new Intent(context, HomeActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(intent);
-                } else {
-                    Toast.makeText(context, "Login incorrecto!", Toast.LENGTH_LONG).show();
-                }
+                WebAuthProvider
+                        .init(account)
+                        .withScope("openid profile email app_metadata")
+                        .start(LoginActivity.this, new AuthCallback() {
+                            @Override
+                            public void onFailure(@NonNull Dialog dialog) {
+                                Toast.makeText(context, "Login incorrecto!", Toast.LENGTH_LONG).show();
+                            }
+
+                            @Override
+                            public void onFailure(AuthenticationException exception) {
+                                Toast.makeText(context, "Login incorrecto!", Toast.LENGTH_LONG).show();
+
+                            }
+
+                            @Override
+                            public void onSuccess(@NonNull Credentials credentials) {
+                                Toast.makeText(context, "Sesión iniciada correctamente!", Toast.LENGTH_LONG).show();
+
+                                sharedPreferences.edit()
+                                        .putString("credential_fitme", gson.toJson(credentials))
+                                        .apply();
+
+                                context.startActivity(intent);
+                            }
+                        });
             }
         });
 
